a9fe883acad44dba92bb1d8d491423c254ee7770
iehuhamp <152163327+iehuhamp@users.noreply.github.com>
2025-03-26 20:48:24 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "./", // Dùng "./" nếu deploy trên custom domain hoặc thư mục con
+  build: {
+    outDir: "dist",
+  },
 });
